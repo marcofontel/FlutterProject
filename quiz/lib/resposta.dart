2b5450bfe03735c76fa4e82cb747bb000d0a32cd
server <wll.rgr@gmail.com>
2022-12-08 14:02:53 -0300

    Chamada de função como parametro do objeto resposta

--- quiz/lib/resposta.dart
+++ quiz/lib/resposta.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
-
-  Resposta(this.texto);
+  final void Function() onSelection;
+  Resposta(this.texto, this.onSelection);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: Text(texto),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.red,
+          ),
+          child: Text(texto),
+          onPressed: onSelection),
     );
   }
 }
