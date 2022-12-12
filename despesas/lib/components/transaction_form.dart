cafdf2bfbe8b19f7790e6a45be3c1e0b2298021a
server <wll.rgr@gmail.com>
2022-12-12 20:50:49 -0300

    Correção formulario - statefulwidget

--- despesas/lib/components/transaction_form.dart
+++ despesas/lib/components/transaction_form.dart
@@ -2,24 +2,32 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
+
   _onPress() {
     final title = titleController.text;
     final value = double.tryParse(valueController.text) ?? 0;
     if (title.isEmpty || value <= 0) {
       return;
     }
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
   final ButtonStyle styleElevatedButton = ElevatedButton.styleFrom(
       textStyle: const TextStyle(fontSize: 20, color: Colors.purple));
+
   @override
   Widget build(BuildContext context) {
     return Card(
